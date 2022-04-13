1abed62b404733f457e0f244d9f665cf8202b760
jazzy <minh15553@gmail.com>
2022-04-13 20:52:09 +0700

    commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// function rsa2se() {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve('resolve')
+//         }, 2000)
+//     })
+// }
+
+// async function ascall() {
+//     console.log('calling');
+//     var res = await rsa2se()
+//     console.log(res);
+// }
+
+// ascall()
+// console.log('has call');
+
+// function mys(...para) {
+//     return para
+// }
+// let a = mys(1, 23, 4)
+// console.log(a);
+
+// const a = [1, 2] 
+// const b = [3, 4]
+// console.log([...a, ...b]);
+
+
+
